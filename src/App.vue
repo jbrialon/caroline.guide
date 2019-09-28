c02b6888f534fd87bf2a586fbd4bcfe9cd11f1b6
Jérém <jeremzor@gmail.com>
2019-09-28 09:51:08 +0200

    basic design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Caroline Brialon
+    </h1>
+    <h2>
+      Accompagnatrice en montagne
+    </h2>
+    <p>
+      "S'eloigner de tout pour se rapprocher de l'essentiel"
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+body {
+  font-size: 1.6rem;
+}
+
 #app {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../src/assets/background.jpg');
+  background-position: center center;
+  background-size: cover;
+  overflow: hidden;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#app h1 {
+  color: white;
+  font-size: 5rem;
+  border: 4px solid white;
+  padding: 1.5rem 5rem;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  text-shadow: 0px 0px 10px #000000;
+}
+
+#app h2 {
+  color: white;
+  font-size: 2.5rem;
+}
+
+#app p {
+  font-style: italic;
+  color: white;
+  font-size: 2.5rem;
 }
 </style>
