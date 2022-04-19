da40ac6fa947cfadf28edd0f4d73a4b30113f853
Jérémy Brialon <jeremzor@gmail.com>
2022-04-19 11:44:22 +0200

    update to vite + vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
   <div id="app">
     <section id="intro">
@@ -33,13 +39,6 @@
     </section>
   </div>
 </template>
-
-<script>
-
-export default {
-  name: 'app'
-}
-</script>
 
 <style>
 html {
