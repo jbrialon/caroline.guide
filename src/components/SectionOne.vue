5b75b6196fcaa28a2c8179bfcacfb4e4e3d78118
Jérémy Brialon <jeremzor@gmail.com>
2023-10-30 14:24:27 +0100

    uppercase issues …

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -1,7 +1,7 @@
 <script setup>
 import image1 from "@/assets/presentation/image-1.jpg";
 import image2 from "@/assets/presentation/image-2.jpg";
-import image3 from "@/assets/presentation/image-3.jpg";
+import image3 from "@/assets/presentation/image-8.jpg";
 </script>
 
 <template>
